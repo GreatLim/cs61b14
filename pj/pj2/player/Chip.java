0e0303586f6a59d81fd92297bbd8a255a84cfdc3
GreatLim <654720229@qq.com>
2018-03-24 17:17:44 +0800

    finish /*module<3>*/ findpair in Chip.java

--- pj/pj2/player/Chip.java
+++ pj/pj2/player/Chip.java
@@ -14,9 +14,9 @@ public class Chip {
 
 
     public Chip(int x, int y, int color){
-        this.color = color;
         this.x = x;
         this.y = y;
+        this.color = color;
     }
 
 
@@ -26,6 +26,128 @@ public class Chip {
      *  @return DList that stores chips that connect with a "this" chip.
      **/
     public DList findPair(Board b) {
-        return null;
+        DList result = new DList();
+        if(color == Color.SPACE) {
+            return null;
+        } else {
+            for(int i = x + 1; i < 8; i++) {
+                int temp = b.grid[i][y].color;
+                if(temp == color) {
+                    result.insertBack(new Chip(i, y , temp));
+                    break;
+                } else if(temp == Color.SPACE) {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+
+            for(int i = x - 1; i >= 0; i--) {
+                int temp = b.grid[i][y].color;
+                if(temp == color) {
+                    result.insertBack(new Chip(i, y , temp));
+                    break;
+                } else if(temp == Color.SPACE) {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+
+            for(int j = y + 1; j < 8; j++) {
+                int temp = b.grid[x][j].color;
+                if(temp == color) {
+                    result.insertBack(new Chip(x, j , temp));
+                    break;
+                } else if(temp == Color.SPACE) {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+
+            for(int j = y - 1; j >= 0; j--) {
+                int temp = b.grid[x][j].color;
+                if(temp == color) {
+                    result.insertBack(new Chip(x, j , temp));
+                    break;
+                } else if(temp == Color.SPACE) {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+
+            for(int i = x + 1, j = y + 1; i < 8 && j < 8; i++, j++) {
+                int temp = b.grid[i][j].color;
+                if(temp == color) {
+                    result.insertBack(new Chip(i, j, temp));
+                    break;
+                } else if(temp == Color.SPACE) {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+
+            for(int i = x - 1, j = y - 1; i >= 0 && j >= 0; i--, j--) {
+                int temp = b.grid[i][j].color;
+                if(temp == color) {
+                    result.insertBack(new Chip(i, j, temp));
+                    break;
+                } else if(temp == Color.SPACE) {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+
+            for(int i = x + 1, j = y - 1; i < 8 && j >= 0; i++, j--) {
+                int temp = b.grid[i][j].color;
+                if(temp == color) {
+                    result.insertBack(new Chip(i, j, temp));
+                    break;
+                } else if(temp == Color.SPACE) {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+
+            for(int i = x - 1, j = y + 1; i >= 0 && j < 8; i--, j++) {
+                int temp = b.grid[i][j].color;
+                if(temp == color) {
+                    result.insertBack(new Chip(i, j, temp));
+                    break;
+                } else if(temp == Color.SPACE) {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+        }
+        return result;
     }
+
+    public String toString(){
+        String s;
+        s = "( " + x + " , " + y + " )";
+        return s;
+    }
+    public static void main(String[] arg) {
+        Board b = new Board();
+        b.grid[4][4].color = Color.WHITE;
+        b.grid[5][5].color = Color.BLACK;
+        b.grid[4][2].color = Color.WHITE;
+        b.grid[4][6].color = Color.WHITE;
+        b.grid[2][2].color = Color.WHITE;
+        b.grid[6][6].color = Color.WHITE;
+        b.grid[6][2].color = Color.WHITE;
+        b.grid[6][4].color = Color.WHITE;
+        b.grid[2][4].color = Color.WHITE;
+        b.grid[2][6].color = Color.WHITE;
+        DList l = b.grid[4][4].findPair(b);
+        System.out.println(l.length());
+    }
+
 }
