2af553c0f2d183a9b10945f1bc549dc0f88d80f6
koko <2416650932@qq.com>
2018-03-24 16:03:19 +0800

    add Chip.bChipCount&Chip.wChipCount

--- pj/pj2/player/Chip.java
+++ pj/pj2/player/Chip.java
@@ -11,6 +11,9 @@ public class Chip {
     public int color;
     public int x;
     public int y;
+    public static int bChipCount = 0;//count the number of black chips used.
+    public static int wChipCount = 0;//count the number of white chips used.
+
 
     public Chip(Move m){
 
