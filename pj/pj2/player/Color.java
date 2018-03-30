4e6db60489565b2dc9f70441d6ae3c96b587930d
GreatLim <654720229@qq.com>
2018-03-30 15:57:01 +0800

    reform code

--- pj/pj2/player/Color.java
+++ pj/pj2/player/Color.java
@@ -9,15 +9,14 @@ public class Color {
     public final static int BLACK = 0;
     public final static int WHITE = 1;
     public final static int SPACE = 2;
-    
-    public static String toString(int color)
-    {
-    		if(color == Color.SPACE) {
-    			return "    ";
-    		}else if(color == Color.WHITE){
-    			return " WW ";
-    		}else {
-    			return " BB ";
-    		}
+
+    public static String toString(int color) {
+        if (color == Color.SPACE) {
+            return "    ";
+        } else if (color == Color.WHITE) {
+            return " WW ";
+        } else {
+            return " BB ";
+        }
     }
-    }
+}
